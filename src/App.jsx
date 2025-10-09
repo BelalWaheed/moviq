e8e4798e101a4fdb113603b568b1c0c9512cc767
belal waheed <belalwaheed11@gmail.com>
2025-10-09 15:51:04 +0300

    Folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Details from "./pages/details/Details";
+import Home from "./pages/home/Home";
+import Movies from "./pages/movies/Movies";
+import NotFound from "./pages/notFound/NotFound";
+import Series from "./pages/series/Series";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <div>
-      <h1>hola</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="series" element={<Series />} />
+        <Route path="details" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
