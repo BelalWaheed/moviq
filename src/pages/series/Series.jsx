08ecc2c9e09f6136cc807a2d2506fe18ae91ff1e
Omar Haidar <omar.heder.store@gmail.com>
2025-10-13 17:53:36 +0300

    pull last update

--- src/pages/series/Series.jsx
+++ src/pages/series/Series.jsx
@@ -14,6 +14,7 @@ import NotFound from "../notFound/NotFound";
 
 function Series() {
     const dispatch = useDispatch();
+    console.log("pull");
 
     const { seriesList, seriesLoading, typing, seriesError } = useSelector(
         state => state.seriesReducer
