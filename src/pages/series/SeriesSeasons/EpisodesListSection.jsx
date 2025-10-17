9d442b94a09aea63f06b2415e0d81baf6c6fb82d
Omar Haidar <omar.heder.store@gmail.com>
2025-10-17 19:49:46 +0300

    fix some bugs in designs

--- src/pages/series/SeriesSeasons/EpisodesListSection.jsx
+++ src/pages/series/SeriesSeasons/EpisodesListSection.jsx
@@ -1,33 +1,41 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import { FaCalendarAlt, FaClock, FaStar } from "react-icons/fa";
-const EpisodesListSection = () => {
-    const { seasonDetails, seasonDetailsLoading, seasonDetailsError } =
-        useSelector(state => state.seriesSeasonsReducer);
 
+const EpisodesListSection = () => {
+    const { seasonDetails } = useSelector(state => state.seriesSeasonsReducer);
     const navigate = useNavigate();
+
     return (
         <section className="max-w-6xl mx-auto px-6 pb-20">
             <h2 className="text-2xl font-bold mb-4 text-red-500">Episodes</h2>
-            <div className="flex gap-6 overflow-x-auto scroll-indicator pb-4">
+            <div className="flex gap-4 scroll-indicator overflow-x-auto overflow-y-hidden pb-4">
                 {seasonDetails?.episodes?.slice(0, 10).map(ep => (
-                    <motion.div
+                    <div
                         key={ep.id}
-                        whileHover={{ scale: 1.05 }}
-                        className="flex-shrink-0 w-64 bg-[#0f0f0f] rounded-2xl overflow-hidden shadow-lg border border-gray-800 transition-transform duration-200 ease-in-out cursor-pointer">
-                        <img
-                            src={
-                                ep.still_path
-                                    ? `https://image.tmdb.org/t/p/w500${ep.still_path}`
-                                    : "/Image-not-found.png"
-                            }
-                            alt={ep.name}
-                            className="w-full h-40 object-cover object-top"
-                        />
+                        className="
+                flex-shrink-0
+                w-[45%] sm:w-[38%] md:w-[28%] lg:w-[22%]
+                bg-[#0f0f0f] rounded-2xl overflow-hidden shadow-lg border border-gray-800 cursor-pointer
+            ">
+                        <div className="relative">
+                            <img
+                                src={
+                                    ep.still_path
+                                        ? `https://image.tmdb.org/t/p/w500${ep.still_path}`
+                                        : "/Image-not-found.png"
+                                }
+                                alt={ep.name}
+                                className="w-full h-40 object-cover object-top"
+                            />
+                            <div className="absolute top-0 left-0 bg-red-600 opacity-70 text-white text-xs font-semibold px-3 py-1 rounded-br-xl">
+                                Ep {ep.episode_number}
+                            </div>
+                        </div>
+
                         <div className="p-3 space-y-2">
-                            <h3 className="text-lg font-bold truncate">
+                            <h3 className="text-lg font-medium truncate">
                                 {ep.name}
                             </h3>
                             <div className="flex items-center gap-2 text-gray-400 text-sm">
@@ -42,16 +50,13 @@ const EpisodesListSection = () => {
                                 <FaClock className="text-red-500" />{" "}
                                 {ep.runtime} min
                             </div>
-                            <p className="text-gray-400 text-sm line-clamp-2">
-                                {ep.overview}
-                            </p>
                         </div>
-                    </motion.div>
+                    </div>
                 ))}
                 {seasonDetails?.episodes.length > 10 && (
                     <button
                         onClick={() => navigate("/AllEpisode")}
-                        className="min-w-[150px] flex flex-col justify-center items-center bg-zinc-800 rounded-2xl hover:bg-red-600 transition-colors duration-300">
+                        className="min-w-[150px] flex flex-col justify-center items-center bg-zinc-800 rounded-2xl cursor-pointer p-4">
                         <span className="text-white text-lg font-bold">
                             +{seasonDetails?.episodes.length - 10}
                         </span>
