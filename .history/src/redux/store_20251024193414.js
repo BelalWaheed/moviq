6e22b33440dcaa79c99bcd15874838dd6435915f
Omar Haidar <omar.heder.store@gmail.com>
2025-10-24 19:49:35 +0300

    udpate file structure of seriesSlices (redux), add SharedSlices (redux)

---
+++ .history/src/redux/store_20251024193414.js
@@ -0,0 +1,43 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { seriesReducer } from "./SeriesSlices/SeriesSlice";
+import { seriesDetailsReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesDetails.js";
+import { mNowReducer } from "./HomeSlices/mNow";
+import { mTopReducer } from "./HomeSlices/mTop";
+import { tNowReducer } from "./HomeSlices/tNow";
+import { tTopReducer } from "./HomeSlices/tTop";
+import { movieDetailsReducer } from "./moviesSlices/getMovieDetails";
+import { seriesTrailerReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesTrailer.js";
+import { searchReducer } from "./HomeSlices/searcheSlice.js";
+import { seriesSeasonsReducer } from "./SeriesSlices/GetRequest/SeriesSeasons/GetSeriesSeasons.js";
+import { seriesSeasonsAggregateCreditsReducer } from "./SeriesSlices/GetRequest/SeriesSeasons/GetSeriesSesonsAggregateCredits.js";
+import { SeriesAggregateCreditsReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesAggregateCredits.js";
+import { PersonReducer } from "./SharedSlices/GetRequest/Person/GetPersonDetails.js";
+import { personCombinedCreditsReducer } from "./SeriesSlices/GetPersonCombinedCredits.js";
+import { SeriesRecommendationsReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesRecommendations.js";
+import { SeriesExternalLinksReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesExternalLinks.js";
+import { SeriesImagesDetailsReducer } from "./SeriesSlices/GetRequest/SeriesDetails/GetSeriesImages.js";
+
+const store = configureStore({
+    reducer: {
+        seriesReducer,
+        seriesDetailsReducer,
+        seriesTrailerReducer,
+        seriesSeasonsReducer,
+        seriesSeasonsAggregateCreditsReducer,
+        SeriesAggregateCreditsReducer,
+        PersonReducer,
+        personCombinedCreditsReducer,
+        SeriesRecommendationsReducer,
+        SeriesExternalLinksReducer,
+        SeriesImagesDetailsReducer,
+
+        mNowReducer,
+        mTopReducer,
+        tNowReducer,
+        tTopReducer,
+        movieDetailsReducer,
+        searchReducer
+    }
+});
+
+export default store;
